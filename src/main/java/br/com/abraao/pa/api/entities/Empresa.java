7b3823634a4ad027b0ebb676cbd827fcf10ec58c
abraao <abraao@localhost>
2018-10-01 13:56:41 -0400

    integração com o lombok

--- src/main/java/br/com/abraao/pa/api/entities/Empresa.java
+++ src/main/java/br/com/abraao/pa/api/entities/Empresa.java
@@ -1,104 +1,67 @@
 package br.com.abraao.pa.api.entities;
 
-
 import java.io.Serializable;
+
 import java.util.Date;
 import java.util.List;
 
-import javax.persistence.CascadeType;
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.FetchType;
-import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.GeneratedValue;
+import javax.persistence.CascadeType;
+import javax.persistence.FetchType;
 import javax.persistence.OneToMany;
 import javax.persistence.PrePersist;
 import javax.persistence.PreUpdate;
+import javax.persistence.Entity;
+import javax.persistence.Column;
 import javax.persistence.Table;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Id;
+
+@Getter
+@Setter
 @Entity
 @Table(name = "empresa")
 public class Empresa implements Serializable {
 
 	private static final long serialVersionUID = 3960436649365666213L;
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Long id;
+
+	@Column(name = "razao_social", nullable = false)
 	private String razaoSocial;
+
+	@Column(name = "cnpj", nullable = false)
 	private String cnpj;
+
+	@Column(name = "data_criacao", nullable = false)
 	private Date dataCriacao;
+
+	@Column(name = "data_atualizacao", nullable = false)
 	private Date dataAtualizacao;
+
+	@OneToMany(mappedBy = "empresa", fetch = FetchType.LAZY, cascade = CascadeType.ALL)
 	private List<Funcionario> funcionarios;
-	
+
 	public Empresa() {
 	}
 
-	@Id
-    @GeneratedValue(strategy=GenerationType.AUTO)
-	public Long getId() {
-		return id;
-	}
-
-	public void setId(Long id) {
-		this.id = id;
-	}
-
-	@Column(name = "razao_social", nullable = false)
-	public String getRazaoSocial() {
-		return razaoSocial;
-	}
-
-	public void setRazaoSocial(String razaoSocial) {
-		this.razaoSocial = razaoSocial;
-	}
-
-	@Column(name = "cnpj", nullable = false)
-	public String getCnpj() {
-		return cnpj;
-	}
-
-	public void setCnpj(String cnpj) {
-		this.cnpj = cnpj;
-	}
-
-	@Column(name = "data_criacao", nullable = false)
-	public Date getDataCriacao() {
-		return dataCriacao;
-	}
-
-	public void setDataCriacao(Date dataCriacao) {
-		this.dataCriacao = dataCriacao;
-	}
-
-	@Column(name = "data_atualizacao", nullable = false)
-	public Date getDataAtualizacao() {
-		return dataAtualizacao;
-	}
-
-	public void setDataAtualizacao(Date dataAtualizacao) {
-		this.dataAtualizacao = dataAtualizacao;
-	}
-
-	@OneToMany(mappedBy = "empresa", fetch = FetchType.LAZY, cascade = CascadeType.ALL)
-	public List<Funcionario> getFuncionarios() {
-		return funcionarios;
-	}
-
-	public void setFuncionarios(List<Funcionario> funcionarios) {
-		this.funcionarios = funcionarios;
-	}
-	
 	@PreUpdate
-    public void preUpdate() {
-        dataAtualizacao = new Date();
-    }
-     
-    @PrePersist
-    public void prePersist() {
-        final Date atual = new Date();
-        dataCriacao = atual;
-        dataAtualizacao = atual;
-    }
+	public void preUpdate() {
+		dataAtualizacao = new Date();
+	}
+
+	@PrePersist
+	public void prePersist() {
+		final Date atual = new Date();
+		dataCriacao = atual;
+		dataAtualizacao = atual;
+	}
 
 	@Override
 	public String toString() {
--- src/main/java/br/com/abraao/pa/api/entities/Lancamento.java
+++ src/main/java/br/com/abraao/pa/api/entities/Lancamento.java
@@ -1,131 +1,77 @@
 package br.com.abraao.pa.api.entities;
 
-
 import java.io.Serializable;
 import java.util.Date;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.EnumType;
-import javax.persistence.Enumerated;
-import javax.persistence.FetchType;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.ManyToOne;
-import javax.persistence.PrePersist;
-import javax.persistence.PreUpdate;
 import javax.persistence.Table;
+import javax.persistence.Entity;
+import javax.persistence.Column;
 import javax.persistence.Temporal;
+import javax.persistence.EnumType;
+import javax.persistence.PreUpdate;
+import javax.persistence.ManyToOne;
+import javax.persistence.FetchType;
+import javax.persistence.PrePersist;
+import javax.persistence.Enumerated;
 import javax.persistence.TemporalType;
+import javax.persistence.GenerationType;
+import javax.persistence.GeneratedValue;
 
 import br.com.abraao.pa.api.enun.TipoEnum;
 
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
 @Entity
 @Table(name = "lancamento")
 public class Lancamento implements Serializable {
-	
+
 	private static final long serialVersionUID = 6524560251526772839L;
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Long id;
+
+	@Temporal(TemporalType.TIMESTAMP)
+	@Column(name = "data", nullable = false)
 	private Date data;
+
+	@Column(name = "descricao", nullable = true)
 	private String descricao;
+
+	@Column(name = "localizacao", nullable = true)
 	private String localizacao;
+
+	@Column(name = "data_criacao", nullable = false)
 	private Date dataCriacao;
+
+	@Column(name = "data_atualizacao", nullable = false)
 	private Date dataAtualizacao;
+
+	@Enumerated(EnumType.STRING)
+	@Column(name = "tipo", nullable = false)
 	private TipoEnum tipo;
+
+	@ManyToOne(fetch = FetchType.EAGER)
 	private Funcionario funcionario;
 
 	public Lancamento() {
 	}
 
-	@Id
-    @GeneratedValue(strategy=GenerationType.AUTO)
-	public Long getId() {
-		return id;
-	}
-
-	public void setId(Long id) {
-		this.id = id;
-	}
-
-	@Temporal(TemporalType.TIMESTAMP)
-	@Column(name = "data", nullable = false)
-	public Date getData() {
-		return data;
-	}
-
-	public void setData(Date data) {
-		this.data = data;
-	}
-
-	@Column(name = "descricao", nullable = true)
-	public String getDescricao() {
-		return descricao;
-	}
-
-	public void setDescricao(String descricao) {
-		this.descricao = descricao;
-	}
-	
-	@Column(name = "localizacao", nullable = true)
-	public String getLocalizacao() {
-		return localizacao;
-	}
-
-	public void setLocalizacao(String localizacao) {
-		this.localizacao = localizacao;
-	}
-
-	@Column(name = "data_criacao", nullable = false)
-	public Date getDataCriacao() {
-		return dataCriacao;
-	}
-
-	public void setDataCriacao(Date dataCriacao) {
-		this.dataCriacao = dataCriacao;
-	}
-
-	@Column(name = "data_atualizacao", nullable = false)
-	public Date getDataAtualizacao() {
-		return dataAtualizacao;
-	}
-
-	public void setDataAtualizacao(Date dataAtualizacao) {
-		this.dataAtualizacao = dataAtualizacao;
-	}
-
-	@Enumerated(EnumType.STRING)
-	@Column(name = "tipo", nullable = false)
-	public TipoEnum getTipo() {
-		return tipo;
-	}
-
-	public void setTipo(TipoEnum tipo) {
-		this.tipo = tipo;
-	}
-
-	@ManyToOne(fetch = FetchType.EAGER)
-	public Funcionario getFuncionario() {
-		return funcionario;
-	}
-
-	public void setFuncionario(Funcionario funcionario) {
-		this.funcionario = funcionario;
-	}
-	
 	@PreUpdate
-    public void preUpdate() {
-        dataAtualizacao = new Date();
-    }
-     
-    @PrePersist
-    public void prePersist() {
-        final Date atual = new Date();
-        dataCriacao = atual;
-        dataAtualizacao = atual;
-    }
+	public void preUpdate() {
+		dataAtualizacao = new Date();
+	}
+
+	@PrePersist
+	public void prePersist() {
+		final Date atual = new Date();
+		dataCriacao = atual;
+		dataAtualizacao = atual;
+	}
 
 	@Override
 	public String toString() {
@@ -135,4 +81,3 @@ public class Lancamento implements Serializable {
 	}
 
 }
-
