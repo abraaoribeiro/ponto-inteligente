cfc4768b6a97d52c3ab3306d63cfc59b4023579d
abraao.ribeiro <abraaohdg@gmail.com>
2018-10-13 13:22:00 -0300

    Adicionado Service de Lancamento

---
+++ src/main/java/br/com/abraao/pa/services/LancamentoService.java
@@ -0,0 +1,52 @@
+package br.com.abraao.pa.services;
+
+import java.util.Optional;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+
+import br.com.abraao.pa.api.entities.Lancamento;
+
+public interface LancamentoService {
+	
+	/**
+	 * Retorna uma lista paginada de um determinado funcionario 
+	 * 
+	 * @param funcionarioId
+	 * @param pageRequest
+	 * @return Page<Lancamento>
+	 */
+	
+	Page<Lancamento> buscarPoFuncionario(Long funcionarioId, PageRequest pageRequest);
+	
+	/**
+	 *Retorna um lacamento por ID
+	 * 
+	 * 
+	 * @param id
+	 * @return Optional<Lancamento>
+	 */
+	
+	Optional<Lancamento> buscarPorId(Long id);
+	
+	/**
+	 *  Persiste um lacamento na base de dados
+	 *   
+	 *   
+	 * @param lancamento
+	 * @return Lancamento
+	 */
+	
+	Lancamento persistir(Lancamento lancamento);
+	
+	/**
+	 * Remove um funcionario por ID
+	 * 
+	 * 
+	 * @param id
+	 */
+	
+	void remover(Long id);
+	
+	
+}
--- src/main/java/br/com/abraao/pa/services/implemets/LancamentosServiceImpl.java
+++ src/main/java/br/com/abraao/pa/services/implemets/LancamentosServiceImpl.java
@@ -1,5 +1,52 @@
 package br.com.abraao.pa.services.implemets;
 
-public interface LancamentosServiceImpl {
+import java.util.Optional;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.stereotype.Service;
+
+import br.com.abraao.pa.api.entities.Lancamento;
+import br.com.abraao.pa.repository.LancamentoRepository;
+import br.com.abraao.pa.services.LancamentoService;
+
+@Service
+public class LancamentosServiceImpl implements LancamentoService {
+
+	private static final Logger log = LoggerFactory.getLogger(LancamentosServiceImpl.class);
+
+	@Autowired
+	private LancamentoRepository lancamentoRepository;
+
+	@Override
+	public org.springframework.data.domain.Page<Lancamento> buscarPoFuncionario(Long funcionarioId,
+			PageRequest pageRequest) {
+
+		log.info("Buscando Lancamento para o funcionario ID {}", funcionarioId);
+		return this.lancamentoRepository.findByFuncionarioId(funcionarioId, pageRequest);
+	}
+
+	@Override
+	public Optional<Lancamento> buscarPorId(Long id) {
+
+		log.info("Buscando um Lancamento por ID {}", id);
+		return Optional.ofNullable(this.lancamentoRepository.findOne(id));
+	}
+
+	@Override
+	public Lancamento persistir(Lancamento lancamento) {
+
+		log.info("Persistindo um lancamento {} ", lancamento);
+		return this.lancamentoRepository.save(lancamento);
+	}
+
+	@Override
+	public void remover(Long id) {
+		log.info("Removendo o lancamento");
+		this.lancamentoRepository.delete(id);
+
+	}
 
 }
