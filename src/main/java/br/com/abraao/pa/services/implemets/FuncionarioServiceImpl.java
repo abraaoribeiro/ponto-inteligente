1ddfa2e40673e89d83ca79dbc71a5c7de61c70fd
abraao.ribeiro <abraaohdg@gmail.com>
2018-10-14 16:24:44 -0300

    Cadastro PF

--- src/main/java/br/com/abraao/pa/services/implemets/FuncionarioServiceImpl.java
+++ src/main/java/br/com/abraao/pa/services/implemets/FuncionarioServiceImpl.java
@@ -5,11 +5,13 @@ import java.util.Optional;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import br.com.abraao.pa.api.entities.Funcionario;
 import br.com.abraao.pa.repository.FuncionarioRepository;
 import br.com.abraao.pa.services.FuncionarioService;
 
+@Service
 public class FuncionarioServiceImpl implements FuncionarioService {
 
 	private static final Logger log = LoggerFactory.getLogger(FuncionarioServiceImpl.class);
